385fd2c964a48e406475eedd07b9af022fefaba7
Student <example@example.com>
2019-06-28 14:19:23 -0700

    install jasmine and Karma and continue to work with 'npm run start'

--- src/business.js
+++ src/business.js
@@ -1,1 +1,9 @@
-// Holds calculations for planet years and runs conversion on user input the returns to user
+// // Holds calculations for planet years and runs conversion on user input the returns to user
+//
+// var leapYear = function(year) {
+//   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
-import './styles.css';
+// import './styles.css';
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+//
+//
+// $(document).ready(function() {
+//   $("form#leap-year").submit(function(event) {
+//     event.preventDefault();
+//     var year = parseInt($("input#year").val());
+//     var result = leapYear(year);
+//
+//     $(".year").text(year);
+//
+//     if (!result) {                 // same as writing if (result === false)
+//       $(".not").text("not");
+//     } else {
+//       $(".not").text("");
+//     }
+//
+//     $("#result").show();
+//   });
+// });
+
 
 //Will take user input age and tell them their age in the years of other planets

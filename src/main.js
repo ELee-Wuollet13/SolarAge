3058a514eff78847d4139154a51ea6c1a2369a0f
Student <example@example.com>
2019-06-28 11:49:58 -0700

    update

--- src/main.js
+++ src/main.js
@@ -1,1 +1,3 @@
+import './styles.css';
+
 //Will take user input age and tell them their age in the years of other planets
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new UglifyJsPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+     title: 'SolarAge',
+     template: './src/index.html',
+     inject: 'body'
+   })
+ ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
